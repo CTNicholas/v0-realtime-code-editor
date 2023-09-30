6d604b4a1b835ed1734ac1e15ed166acb922f1f1
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-09-30 16:53:11 +0100

    feat: Responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <Room>
       <div className="w-screen h-screen bg-gradient-to-r from-purple-400 via-blue-500 to-green-500 flex items-center justify-center">
-        <div className="bg-[#000000] rounded-lg p-4 h-[270px] w-[600px] mx-auto my-auto shadow-2xl">
+        <div className="bg-[#000000] rounded-lg p-4 min-h-[270px] w-[600px] mx-auto my-auto shadow-2xl">
           <div className="flex items-center justify-between text-[#808080]">
             <div className="flex items-center gap-2">
               <div className="h-3 w-3 rounded-full bg-[#FF2D55]" />
